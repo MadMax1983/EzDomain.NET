4fb8fe3aacb0efffe6cf0962a96fe3eab34c9e70
Mad Max <maciek.korzeniewski@gmail.com>
2021-10-02 13:21:44 +0200

    Created project structure

---
+++ src/EzDomain.EventSourcing.TestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EzDomain.EventSourcing.TestApp
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
